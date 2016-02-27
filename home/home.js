8dc61ecb1b011bde79644a52fa222400751daf12
Greenjam94 <Greenjam94@gmail.com>
2016-02-27 13:17:53 -0500

    Add meet form on the homepage

--- home/home.js
+++ home/home.js
@@ -9,50 +9,6 @@ cogss.controller("HomeCtrl", ["$scope", "$http", function ($scope, $http) {
         home.meets = res.data;
     });
 
-    cogss.controller("addMeetModalCtrl", ["$scope", "$uibModal", "$http", function ($scope, $uibModal, $http) {
-        "use strict";
-        var main = this;
-
-        $scope.animationsEnabled = true;
-
-        $scope.openAddMeetModal = function (size) {
-            var modalInstance = $uibModal.open({
-                animation: $scope.animationsEnabled,
-                templateUrl: 'addMeetModalContent.html',
-                controller: 'addMeetModalInstanceCtrl',
-                size: size
-            });
-
-            modalInstance.result.then(function (selectedItem) {
-                $scope.selected = selectedItem;
-            });
-        };
-    }]);
-
-    cogss.controller('addMeetModalInstanceCtrl', function ($scope, $uibModalInstance, $http) {
-
-        $scope.email = "";
-        $scope.password = "";
-
-        $scope.ok = function () {
-
-            console.log("Clicked add meet");
-            //var user = {email:$scope.email, password: $scope.password};
-            //$http.post("/login", JSON.stringify(user)).then(function (res) {
-            //    if (200 == res.status) {
-            //        $scope.auth = "true";
-            //    } else {
-            //        $scope.auth = "false";
-            //    }
-            //});
-            $uibModalInstance.close();
-        };
-
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
-    });
-
     //home.logout = function(){
     //    $http.post("/logout").then(function(res){
     //        if (res.status === 200) {
@@ -73,3 +29,78 @@ cogss.controller("HomeCtrl", ["$scope", "$http", function ($scope, $http) {
     //    });
     //}
 }]);
+
+cogss.controller("addMeetModalCtrl", ["$scope", "$uibModal", "$http", function ($scope, $uibModal, $http) {
+    "use strict";
+    var main = this;
+
+    $scope.animationsEnabled = true;
+
+    $scope.openAddMeetModal = function (size) {
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'addMeetModalContent.html',
+            controller: 'addMeetModalInstanceCtrl',
+            size: size
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        });
+    };
+}]);
+
+cogss.controller('addMeetModalInstanceCtrl', function ($scope, $uibModalInstance, $http) {
+
+    $scope.host = "";
+    $scope.name = "";
+    $scope.location = "";
+    $scope.date = "";
+    $scope.public = "";
+    $scope.createdBy = 0;
+    $scope.createdOn = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+    $scope.ok = function () {
+
+        //ToDo: Validation
+        //if (!$scope.name.match(/^[a-zA-Z0-9]+$/)) {
+        //    alert('Bad name: '+ $scope.name);
+        //    return;
+        //}
+        //if (!$scope.host.match(/^[a-zA-Z\s]+$/)) {
+        //    alert('Bad Name');
+        //    return;
+        //}
+        //if (!$scope.location.match(/^[a-zA-Z\s,.]+$/)) {
+        //    alert('Bad location');
+        //    return;
+        //}
+        //if (!$scope.date.match(/^[0-9/]+$/)) {
+        //    alert('Bad date');
+        //    return;
+        //}
+        //if (!$scope.public.match(/^[01]+$/)) {
+        //    alert('Bad public');
+        //    return;
+        //}
+
+        var meet = {
+            host: $scope.host,
+            name: $scope.name,
+            location: $scope.location,
+            date: $scope.date,
+            public: $scope.public,
+            createdBy: $scope.createdBy,
+            createdOn: $scope.createdOn
+        };
+
+        $http.post('/meets', JSON.stringify(meet)).then(function(res){
+            console.log(res);
+        });
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
