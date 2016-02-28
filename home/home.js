a6fc246142a704c23fa76632d686d24376c7f56f
Greenjam94 <Greenjam94@gmail.com>
2016-02-28 02:58:35 -0500

    Cleaning up home directory

--- home/home.js
+++ home/home.js
@@ -2,37 +2,15 @@ cogss.controller("HomeCtrl", ["$scope", "$http", function ($scope, $http) {
     "use strict";
 
     var home = this;
-
     home.meets = {};
 
     $http.get("/meets").then(function(res) {
         home.meets = res.data;
     });
-
-    //home.logout = function(){
-    //    $http.post("/logout").then(function(res){
-    //        if (res.status === 200) {
-    //            window.location.href  = "#/login";
-    //        } else {
-    //            console.log("Error on logout", res);
-    //        }
-    //    });
-    //};
-
-    //home.submit = function(){
-    //    console.log("login!");
-    //
-    //    $http.post("/login", home.user).then(function(res){
-    //        window.location.href = "#/";
-    //    }, function(){
-    //        alert("login failed");
-    //    });
-    //}
 }]);
 
-cogss.controller("addMeetModalCtrl", ["$scope", "$uibModal", "$http", function ($scope, $uibModal, $http) {
+cogss.controller("addMeetModalCtrl", ["$scope", "$uibModal", function ($scope, $uibModal) {
     "use strict";
-    var main = this;
 
     $scope.animationsEnabled = true;
 
@@ -50,7 +28,7 @@ cogss.controller("addMeetModalCtrl", ["$scope", "$uibModal", "$http", function (
     };
 }]);
 
-cogss.controller('addMeetModalInstanceCtrl', function ($scope, $uibModalInstance, $http) {
+cogss.controller('addMeetModalInstanceCtrl', ["$scope", "$uibModalInstance", function ($scope, $uibModalInstance) {
 
     $scope.host = "";
     $scope.name = "";
@@ -62,27 +40,7 @@ cogss.controller('addMeetModalInstanceCtrl', function ($scope, $uibModalInstance
 
     $scope.ok = function () {
 
-        //ToDo: Validation
-        //if (!$scope.name.match(/^[a-zA-Z0-9]+$/)) {
-        //    alert('Bad name: '+ $scope.name);
-        //    return;
-        //}
-        //if (!$scope.host.match(/^[a-zA-Z\s]+$/)) {
-        //    alert('Bad Name');
-        //    return;
-        //}
-        //if (!$scope.location.match(/^[a-zA-Z\s,.]+$/)) {
-        //    alert('Bad location');
-        //    return;
-        //}
-        //if (!$scope.date.match(/^[0-9/]+$/)) {
-        //    alert('Bad date');
-        //    return;
-        //}
-        //if (!$scope.public.match(/^[01]+$/)) {
-        //    alert('Bad public');
-        //    return;
-        //}
+        //Validation?
 
         var meet = {
             host: $scope.host,
@@ -97,10 +55,11 @@ cogss.controller('addMeetModalInstanceCtrl', function ($scope, $uibModalInstance
         $http.post('/meets', JSON.stringify(meet)).then(function(res){
             console.log(res);
         });
+
         $uibModalInstance.close();
     };
 
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
-});
+}]);
