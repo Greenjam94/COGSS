064615baa683cc675788625a039e384a9fa12f7b
Greenjam94 <Greenjam94@gmail.com>
2016-03-15 19:14:07 -0400

    Correct team scoring

--- server.js
+++ server.js
@@ -217,17 +217,19 @@ app.put("/gymnasts", function (req, res) {
         return;
     }
 
-    //send
+    //update event score
     connection.query(
         'UPDATE gymnasts SET '+req.body.eventName+'='+req.body.eventScore+' WHERE gymnastID = '+req.body.id,
         function(err) {
             if (err) throw err;
+            //calculate gymnast's all around
             connection.query(
-                'SELECT teamID, meetID, wVault, wBars, wBeam, wFloor, mFloor, mPommel, mRings, mVault, mParallel, mHigh' +
+                'SELECT teamID, meetID, gender, wVault, wBars, wBeam, wFloor, mFloor, mPommel, mRings, mVault, mParallel, mHigh' +
                 ' FROM gymnasts' +
                 ' WHERE gymnastID = '+req.body.id,
                 function(err,rows) {
                     if (err) throw err;
+                    var gender = rows[0].gender;
                     var allAround = rows[0].wVault +
                                     rows[0].wBars +
                                     rows[0].wBeam +
@@ -244,19 +246,41 @@ app.put("/gymnasts", function (req, res) {
                         'UPDATE gymnasts SET score = '+allAround+' WHERE gymnastID = '+req.body.id,
                         function(err) {
                             if (err) throw err;
-                            connection.query(
-                                'UPDATE teams '+
-                                'SET Score = '+
-                                    '(SELECT SUM(score) '+
-                                    'FROM gymnasts '+
-                                    'WHERE meetID = '+meetID+
-                                    ' AND teamID = '+teamID+')'+
-                                ' WHERE teamID ='+teamID,
-                                function (err) {
-                                    if (err) throw err;
-                                    res.status(200).send(JSON.stringify(allAround));
-                                }
-                            );
+                            if (gender == 0) {
+                                //update women's team score
+                                connection.query(
+                                    'UPDATE teams '+
+                                    '   SET Score = '+
+                                    'SELECT SUM(wVault)+SUM(wBars)+SUM(wBeam)+SUM(wFloor)' +
+                                    '  FROM gymnasts' +
+                                    ' WHERE meetID = '+ meetID +
+                                    '   AND teamID = '+ teamID +
+                                    ' LIMIT 4)' +
+                                    ' WHERE teamID ='+teamID,
+
+                                    function (err) {
+                                        if (err) throw err;
+                                        res.status(200).send(JSON.stringify(allAround));
+                                    }
+                                );
+                            } else if (gender == 1) {
+                                //update men's team score
+                                connection.query(
+                                    'UPDATE teams '+
+                                    '   SET Score = '+
+                                    'SELECT SUM(mFloor)+SUM(mPommel)+SUM(mRings)+SUM(mVault)+SUM(mParallel)+SUM(mHigh)' +
+                                    '  FROM gymnasts' +
+                                    ' WHERE meetID = '+ meetID +
+                                    '   AND teamID = '+ teamID +
+                                    ' LIMIT 3)' +
+                                    ' WHERE teamID ='+teamID,
+
+                                    function (err) {
+                                        if (err) throw err;
+                                        res.status(200).send(JSON.stringify(allAround));
+                                    }
+                                );
+                            }
                         }
                     );
                 }
